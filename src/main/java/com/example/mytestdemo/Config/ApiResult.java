c545a1cd7ce775fb64a7bd95e7f4a124cf76aed0
angtai <angtai@maihaoche.com>
2020-09-10 17:07:35 +0800

    日志补充

--- src/main/java/com/example/mytestdemo/Config/ApiResult.java
+++ src/main/java/com/example/mytestdemo/Config/ApiResult.java
@@ -3,7 +3,10 @@ package com.example.mytestdemo.Config;
 import lombok.Builder;
 import lombok.Data;
 
-
+/**
+ * 全局统一数据结构返回
+ * @param <T>
+ */
 @Data
 @Builder
 public class ApiResult<T> {
@@ -12,6 +15,7 @@ public class ApiResult<T> {
     private T data;
     private String message;
 
+    //静态方法需要在返回对象前加泛型
     public static <T>ApiResult<T> ok(T t, String message){
         return (ApiResult<T>) ApiResult.builder().code(200).data(t).message(message).success(true).build();
     }
