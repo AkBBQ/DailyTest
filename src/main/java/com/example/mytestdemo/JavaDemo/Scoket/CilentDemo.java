b6a8c5edd7d215eea9007aa582f0e6b3c531d6e9
angtai <angtai@maihaoche.com>
2020-10-22 17:17:11 +0800

    Socket 客户端 服务端 demo

---
+++ src/main/java/com/example/mytestdemo/JavaDemo/Scoket/CilentDemo.java
@@ -0,0 +1,39 @@
+package com.example.mytestdemo.JavaDemo.Scoket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * socket测试
+ *
+ * @author angtai
+ */
+
+public class CilentDemo {
+    public static void main(String[] args) {
+        try {
+            // 创建 Socket 用来发起请求，设置请求 IP 为本机，端口号为 8080
+            Socket socket = new Socket("127.0.0.1", 3300);
+            // 通过 Socket 的流对象创建 PrintWriter 用于发送请求数据，创建 BufferedReader 用于接收服务端响应数据
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // 发送数据
+            String requestData = " 客户端请求数据 ";
+            printWriter.println(requestData);
+            printWriter.flush();
+            // 接收服务端响应数据
+            String responseData = bufferedReader.readLine();
+            System.out.println(" 接收到服务端的响应数据为： " + responseData);
+
+            // 关闭资源
+            printWriter.close();
+            bufferedReader.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/mytestdemo/JavaDemo/Scoket/ServeDemo.java
@@ -0,0 +1,38 @@
+package com.example.mytestdemo.JavaDemo.Scoket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 服务器
+ */
+
+public class ServeDemo {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3300);
+            // 调用 accept() 方法监听客户端请求，该方法是阻塞方法，程序会停留在这里直到有客户端请求服务端的 3300 接口
+            //Socket 用于通信中的数据传输
+            Socket socket = serverSocket.accept();
+            //从socket中拿到输入流
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String requestData = bufferedReader.readLine();
+            System.out.println("请求数据为:" + requestData);
+
+            // 创建 PrintWriter 发送服务端响应数据，接收数据流对象也来自 Socket
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream()) ;
+            // 写入响应数据
+            printWriter.println( " 服务端已接收到你的请求，响应数据为（服务端响应数据）！ " ) ;
+            // 使用 flush() 方法强制发送数据而不是等到缓冲区满了后才发送
+            printWriter.flush() ;
+            // 关闭资源
+            printWriter.close() ;
+            bufferedReader.close() ;
+            socket.close() ;
+            serverSocket.close() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
