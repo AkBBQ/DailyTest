433a3cf8e183a13c147668f899ae1f185fcc62a8
sunjie <sunjie@hd123.com>
2023-02-14 11:44:49 +0800

    threadLocal demo fix

--- src/main/java/com/example/mytestdemo/HighJavaDemo/JUC/xiancheng/ThreadLocalDemo.java
+++ src/main/java/com/example/mytestdemo/HighJavaDemo/JUC/xiancheng/ThreadLocalDemo.java
@@ -1,19 +1,28 @@
 package com.example.mytestdemo.HighJavaDemo.JUC.xiancheng;
 
 /**
- * All rights Reserved, Designed By www.maihaoche.com
- *
- * @Package com.example.mytestdemo.HighJavaDemo.JUC.xiancheng
- * @author: angtai（angtai@maihaoche.com）
- * @date: 2021/1/21 6:52 下午
- * @Copyright: 2017-2020 www.maihaoche.com Inc. All rights reserved.
+ set key为当前线程
  */
 
 public class ThreadLocalDemo {
 
     public static void main(String[] args) {
         ThreadLocal<String> threadLocal = new ThreadLocal<String>();
-        threadLocal.set("haha");
+
+        new Thread(()->{
+            threadLocal.set("子线程");
+            System.out.println(threadLocal.get());
+            threadLocal.remove();
+        }).start();
+
+        threadLocal.set("主线程");
         System.out.println(threadLocal.get());
+        threadLocal.remove();
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
