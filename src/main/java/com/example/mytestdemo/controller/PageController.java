4561851eb29378a4b38bc8511fcae8ea2c045db9
sunjie <sunjie@hd123.com>
2023-07-17 18:10:32 +0800

    pagehelper分页线程污染坑

--- src/main/java/com/example/mytestdemo/controller/PageController.java
+++ src/main/java/com/example/mytestdemo/controller/PageController.java
@@ -1,6 +1,10 @@
 package com.example.mytestdemo.controller;
 
+import com.example.mytestdemo.Command.User;
 import com.example.mytestdemo.Config.ApiResult;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import feign.Param;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,6 +62,26 @@ public class PageController {
     @ResponseBody
     public ApiResult<?> adminUpdate() {
         return ApiResult.ok("管理员更新");
+    }
+
+
+    @GetMapping("/test/page")
+    public List<User> selectAll(@Param("pageNum") int pageNum, @Param("pageSize") int pageSize) {
+        //starePage方法会将分页信息存放到本线程ThreadLocal中，如果像如下这种写法 那么不执行查询情况下 污染了线程 导致下一次查询被玷污
+
+        //解决方案
+        //CASE 1 PageHelper对象不共用
+        //CASE 2 PageHelper.clearPage();
+        //CASE 3 确保 PageHelper.startPage 之后执行查询
+        PageHelper.startPage(pageNum, pageSize);
+        if (pageNum > 100){
+//        List<User> users = userMapper.selectAll();
+        }
+        List<User> users = new ArrayList<>();
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+
+        PageHelper.clearPage();
+        return pageInfo.getList();
     }
 
 
